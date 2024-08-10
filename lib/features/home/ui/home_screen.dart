5a1eaa0d5494e8e7ef87b6559c763ad138bc6b8c
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-08-10 12:53:47 +0300

    - Add dark mode support to app system par - impement Home top par - Add colors and styles

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/home_top_bar.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          margin: const EdgeInsets.fromLTRB(16, 12, 16, 28),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [HomeTopBar()],
+          ),
+        ),
       ),
     );
   }
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_adv_course_2/core/DI/dependency_injection.dart';
 import 'package:flutter_adv_course_2/core/Routing/app_router.dart';
 import 'package:flutter_adv_course_2/doc_app.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() async{
+void main() async {
   setupGetIt();
   await ScreenUtil.ensureScreenSize();
+  // Set the color of the status bar
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.white, // Set the color here
+    statusBarIconBrightness:
+        Brightness.dark, // Set the brightness of the status bar icons
+  ));
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
